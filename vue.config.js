0a3398c8ea946a3f213b7119289574f6157f60a3
sajadadineh <sajadadineh@protonmail.com>
2020-09-04 04:28:58 +0430

    vue deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  // See https://stackoverflow.com/questions/57916549/vue-cli-run-build-typeerror-name-undefined
-  css: {
-    sourceMap: true
-  },
-
-  publicPath: "",
-  chainWebpack: config => {
-    config.module
-      .rule("eslint")
-      .use("eslint-loader")
-      .options({ fix: true });
-  }
+  publicPath: process.env.NODE_ENV === "production" ? "/my-site/" : "/",
 };
